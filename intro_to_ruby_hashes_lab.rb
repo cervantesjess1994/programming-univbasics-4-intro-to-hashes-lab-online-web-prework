70a71bf9f8f4a8a70c6a71d154c37601ec6f7e64
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-06 08:47:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  has.new
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  beverages { juice:"apple", soda:"coke"}
 end
 
 def pioneer
